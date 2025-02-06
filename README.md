9edf331f156791f2a6c7668e7c0e28b83ad3dfc8
Tomas Molina <jtml.mass@gmail.com>
2025-02-05 18:45:30 -0600

    Notes and updates for Practice source

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # springboot-graphql-test
+
+Studies of GraphQL on SpringBoot inspired by the [Spring GraphQL](https://spring.io/guides/gs/graphql-server/) guide.
---
+++ src/main/java/com/dialexa/springbootgraphqltest/SpringbootGraphqlTestApplication.java
@@ -0,0 +1,13 @@
+package com.dialexa.springbootgraphqltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGraphqlTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootGraphqlTestApplication.class, args);
+    }
+
+}
